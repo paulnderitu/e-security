c915aa1f427e3da71e72b807d80c0ff65da5be5d
Your Name <youremail@domain.com>
2017-06-19 16:11:23 +0300

    added an item update component

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateProductForm: false,
+
   actions: {
+    update(product, params) {
+      this.sendAction('update', product, params);
+    },
+
     delete(product) {
-      if(confirm('Are you sure you want to delete this product?')){
+      if (confirm('Are you sure you want to delete this product?'))
+      {
         this.sendAction('destroyProduct', product);
       }
     }
