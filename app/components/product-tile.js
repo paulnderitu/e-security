1e475dba094b1938f68b088ef97fe28d9fd36ca3
Your Name <youremail@domain.com>
2017-06-19 13:54:52 +0300

    added the delete component functionality

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(product) {
+      if(confirm('Are you sure you want to delete this product?')){
+        this.sendAction('destroyProduct', product);
+      }
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('product');
   },
+
+  actions: {
+    destroyProduct(product) {
+      product.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
