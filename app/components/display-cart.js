d6ec97683ed48f8f0703c6a82404c3be791e660a
Your Name <youremail@domain.com>
2017-06-20 20:18:34 +0300

    diplayed cart elements

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+  actions: {
+    removeProduct(product) {
+      this.get('shoppingCart').remove(product)
+    }
+  }
 });
