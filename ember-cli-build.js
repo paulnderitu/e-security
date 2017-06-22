640841c73d1320cb3bf31353672660658762f549
Nauphrage <nauphragecompilers@gmail.com>
2017-06-22 09:00:15 +0300

    created another branch

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,10 +3,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    'ember-bootstrap': {
-      'bootstrapVersion': 3,
-      'importBootstrapFont': true,
-      'importBootstrapCSS': false
+'ember-bootstrap': {
+  'bootstrapVersion': 3,
+  'importBootstrapFont': true,
+  'importBootstrapCSS': false
     }
   });
 
